586aaefceec692476bb2319e8ec4b45acae14b6e
Ruslan Vasiliev <ervasive@gmail.com>
2024-11-12 13:50:55 +0200

    feat: create PostFactory class for generating post data

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Enums\PostStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'body',
